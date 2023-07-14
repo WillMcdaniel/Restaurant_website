4eeec50f2d20a4c64aaed99fc6d172b62ba4ba9f
WillMcdaniel <125214391+WillMcdaniel@users.noreply.github.com>
2023-07-13 23:43:49 -0700

    Refactoring the routes

--- Server.py
+++ Server.py
@@ -3,19 +3,19 @@ app = Flask(__name__)
 
 #this is the main route
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def home():
     return render_template("index.html")
 
-@app.route('/index.html')
+@app.route('/index.html',methods=['GET','POST'])
 def homepage():
     return render_template("index.html")
 
 
-@app.route('/threecolumn.html')
+@app.route('/threecolumn.html',methods=['GET','POST'])
 def threecolumn():
     return render_template("threecolumn.html")
 
 
-if (__name__) == "__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=8080)
